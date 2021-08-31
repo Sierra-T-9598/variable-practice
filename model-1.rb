d6edcfa689e1f772d675776c44edc3ac95f564be
Sierra Tucker <sierra.tucker121@gmail.com>
2021-08-31 10:58:33 -0600

    Assign variables to arrays

--- model-1.rb
+++ model-1.rb
@@ -9,3 +9,8 @@ number_of_schools = 3
 busy_trailheads = true
 has_breakfast_place = false
 great_views = true
+
+restaurants = ['Mainstage Brewing', 'Oskar Blues', 'La Mariposa', 'Barking Dog
+  Cafe']
+parks = ['Lavern Johnson','Bohn','Sandstone']
+local_wildlife = ['black bears', 'deer', 'mountain lions', 'rattle snakes']
